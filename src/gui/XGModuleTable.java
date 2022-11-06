8bbc9064d8cec0b8c94f62eb2866411c25f7559c
thomas <thomas@probook>
2022-11-06 18:38:18 +0100

    TableSelection implemented;

--- src/gui/XGModuleTable.java
+++ src/gui/XGModuleTable.java
@@ -1,11 +1,11 @@
 package gui;
-import module.*;
+import static application.XGLoggable.LOG;import module.*;import value.XGValue;
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;import javax.swing.event.ListSelectionListener;import javax.swing.table.*;
+import javax.swing.event.ListSelectionEvent;import javax.swing.event.ListSelectionListener;import javax.swing.event.TableModelEvent;import javax.swing.event.TableModelListener;import javax.swing.table.*;
 import java.awt.*;
-import java.util.HashSet;import java.util.Set;
+import java.util.Arrays;import java.util.HashSet;import java.util.Set;
 
-public class XGModuleTable extends JTable implements java.awt.event.MouseListener
+public class XGModuleTable extends JTable
 {
 	private static final int GAP = 12;
 
@@ -14,7 +14,9 @@ public class XGModuleTable extends JTable implements java.awt.event.MouseListene
 	public XGModuleTable(XGModuleType type)
 	{	super(new XGModuleTableModel(type));
 		this.getModel().addTableModelListener(this);
-		this.addMouseListener(this);
+		this.setCellSelectionEnabled(true);
+		this.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		this.getSelectionModel().addListSelectionListener(this);
 		this.setAutoCreateRowSorter(true);
 		this.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 		this.setRowHeight(this.getFont().getSize() + GAP);
@@ -32,26 +34,35 @@ public class XGModuleTable extends JTable implements java.awt.event.MouseListene
 		return c;
 	}
 
-	public XGModule getSelectedModule()//nur das erste selektierte
-	{	if(this.getModel() instanceof XGModuleTableModel)
-		{	XGModuleTableModel model = (XGModuleTableModel)this.getModel();
-			for(int r : this.getSelectedRows())
-			{	Object o = model.getValueAt(this.getRowSorter().convertRowIndexToModel(r), 0);
-				if(o instanceof XGModule) return (XGModule)o;
+	@Override public void valueChanged(ListSelectionEvent e)//ListSelectionListener
+	{	super.valueChanged(e);
+		if(e.getValueIsAdjusting()) return;
+		String valueString = "";
+		boolean answered = false;
+
+		for(int col : this.getSelectedColumns())
+		{	for(int r : this.getSelectedRows())
+			{	int selRow = this.getRowSorter().convertRowIndexToModel(r);
+				Object o = this.getModel().getValueAt(selRow, col);
+				if(o instanceof XGModule)
+				{	XGEditWindow.getEditWindow((XGModule)o).setVisible(true);
+					this.clearSelection();
+					continue;
+				}
+				if(o instanceof XGValue)
+				{	if(!answered)
+					{	valueString = JOptionPane.showInputDialog(this, this.getColumnName(col), 0);
+						answered = true;
+					}
+					try
+					{	((XGValue)o).setValue(valueString, true);
+					}
+					catch(NumberFormatException ex)
+					{	LOG.warning(ex.getMessage());
+					}
+				}
 			}
 		}
-		return null;
+		this.clearSelection();
 	}
-
-	public void mouseClicked(java.awt.event.MouseEvent event)
-	{	XGEditWindow.getEditWindow(this.getSelectedModule()).setVisible(true);
-	}
-
-	public void mousePressed(java.awt.event.MouseEvent event){}
-
-	public void mouseReleased(java.awt.event.MouseEvent event){}
-
-	public void mouseEntered(java.awt.event.MouseEvent event){}
-
-	public void mouseExited(java.awt.event.MouseEvent event){}
 }
